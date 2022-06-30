21ae8c826d3297f264f8f88afa55df3463c2beea
jamiu <kabirm474@gmail.com>
2022-06-30 12:26:45 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio>
+/**
+ * main - entry
+ * return-Always 0(success)
+ * */
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+
