6ce3ef228075367f134ac377791cdfdafd777367
jamiu <kabirm474@gmail.com>
2022-06-30 13:29:00 -0700

    print put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  *
  * main - print the string in the put function
-   * Description: using the main function
-   * this program prints "Programming is like building a multilingual puzzle
-  *  Return: 0
-  * */
+ *  Description: using the main function
+ * this program prints Programming is like building a multilingual puzzle
+ *  Return: 0
+   */
 
 
 
