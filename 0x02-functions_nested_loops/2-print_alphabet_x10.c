a11b927ef73f8d504229b8c3b054d34bbebb7a4b
jamiu <kabirm474@gmail.com>
2022-07-05 08:55:20 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,12 @@
 /**
  * print_alphabet_x10 - prints alphabet 10 times
  * Return:void
- */
-void print_alphabet_10(void)
+ **/
+void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
+
 	while (i < 10)
 	{
 		c = 'a';
