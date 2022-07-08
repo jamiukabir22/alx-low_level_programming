16611453a53dbd84413abc025953ea0bb45e2154
jamiu <kabirm474@gmail.com>
2022-07-07 21:33:42 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#inlclude"main.h"
+/**
+ * _isupper - check for upper case letter
+ * @c : charater to check for upper case
+ * Return:0 or 1
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
