0b0810b72e5acb4809296ba614b37e3ca112f7fa
jamiu <kabirm474@gmail.com>
2022-07-11 06:27:36 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - begin here
+ *
+ * Return: 0 is successful
+ */
+int main(void)
+{
+	char k = 0;
+	int i = 0;
+	/* your code goes there */
+
+		while (i < 10)
+		{
+			putchar(k);
+
+					k++;
+
+							i++;
+		}
+		putchar('\n');
+		return (0);
+}
