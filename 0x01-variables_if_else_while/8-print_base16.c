33d7b4a223a2d54b5a71e818a9fa44968aa212a0
jamiu <kabirm474@gmail.com>
2022-07-11 07:00:29 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - begins here
+ * Return: 0 success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+		int i = 48;
+
+			/* your code goes there */
+
+			while (i < 58)
+			{
+				putchar(i);
+
+						i++;
+			}
+			i = 0;
+
+				while (i < 6)
+				{
+					putchar(ch);
+
+							ch++;
+
+									i++;
+				}
+				putchar('\n');
+					return (0);
+}
