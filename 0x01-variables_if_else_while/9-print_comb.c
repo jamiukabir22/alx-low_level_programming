8e0b74d0d1e94288577274026164255488a9fa99
jamiu <kabirm474@gmail.com>
2022-07-11 07:29:23 -0700

    main header

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,27 +2,25 @@
 #include<stdlib.h>
 /**
  * main - begins here
- * prints all single digits of base 10
+ * Description: prints all single digits of base 10
  * starts from 0, followed by a new line
  * Return: 0
  */
 int main(void)
 {
 	int c = 0;
-
 		/* code goes there */
-
-		while (c < 1)
+	while (c < 1)
+	{
+		putchar(48 + c);
+		if (c != 9)
 		{
-			putchar(48 + c);
-				if (c != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					c++;
+			putchar(',');
+			putchar(' ');
 		}
-		putchar('\n');
+		c++;
+		}
+	putchar('\n');
 	return (0);
 }
 
