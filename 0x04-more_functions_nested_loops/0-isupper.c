f5c10fe788cdd22d726d86e4b518e8631ab76bb9
jamiu <kabirm474@gmail.com>
2022-07-11 07:38:32 -0700

    main header

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,17 @@
-#inlclude"main.h"
+#include"main.h"
+
 /**
  * _isupper - check for upper case letter
- * @c : charater to check for upper case
+ * @c : character to check the case
  * Return:0 or 1
  */
+
 int _isupper(int c)
+
 {
-	return (c >= 65 && c <= 90);
+
+		return (c >= 65 && c <= 90);
+
+
+
 }
