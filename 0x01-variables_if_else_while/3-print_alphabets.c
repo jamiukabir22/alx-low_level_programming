656ca587f74654647faecf91ab778965e2bfe992
jamiu <kabirm474@gmail.com>
2022-07-11 06:04:41 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char a2z;
 
-	for (a2z = 'a'; a2z <= 'z'; a2z++);
+	for (a2z = 'a'; a2z <= 'z'; a2z++)
 	{
 		putchar(a2z);
-	}
-	for (a2z = 'A'; a2z <= 'Z'; a2z++);
+
+	for (a2z = 'A'; a2z <= 'Z'; a2z++)
 	{
 		putchar(a2z);
 	}
