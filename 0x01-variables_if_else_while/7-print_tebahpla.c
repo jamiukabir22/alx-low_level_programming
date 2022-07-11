97d6a781dd689f75e6b8da7dd6c16089107c1399
jamiu <kabirm474@gmail.com>
2022-07-11 06:54:49 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - begins here
+ * Return: 0 succes
+ */
+int main(void)
+{
+	char ch = 'z';
+
+		int i = 26;
+
+			/* your code goes there */
+
+			while (i > 0)
+			{
+				putchar(ch);
+
+						ch--;
+
+								i--;
+			}
+			putchar('\n');
+
+				return (0);
+}
