31438a8488d657e89955f91ed895b1474afa4ae6
jamiu <kabirm474@gmail.com>
2022-07-13 00:05:05 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - prints the length of a string
+ * @s: shows the array with the characters of the word
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	       ;
+		i = 0;
+half = len / 2;
+while (half--)
+{
+	temp = s[len - i - 1];
+	s[len - i - 1] = s[i];
+	s[i] = temp;
+	i++;
+}
+}
