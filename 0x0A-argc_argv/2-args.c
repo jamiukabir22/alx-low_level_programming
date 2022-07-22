59cdee868d3de93ded258a1dec5bf0f0121f0015
jamiu <kabirm474@gmail.com>
2022-07-22 05:00:40 -0700

    print argument on new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints number of argument passed to the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
