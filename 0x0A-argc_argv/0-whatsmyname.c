1da2a42d9ad53d97ffe0ecd7df826414d7ec247a
jamiu <kabirm474@gmail.com>
2022-07-22 04:47:36 -0700

    prints name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints progarm's name, follwed by new line
+ * @argc: int
+ * @argv: int
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
