ed935c358b9120bd944ed8cc5a7609e20dc7a18c
jamiu <kabirm474@gmail.com>
2022-07-22 05:23:48 -0700

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the addition, followed by new line
+ * @s: string to check
+ * Return: 1 or 0
+ */
+int isInteger(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/**
+ * main - adds positive number
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+
+	while (--argc)
+	{
+		if (isInteger(argv[argc]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[argc]);
+	}
+	printf("%i\n", sum);
+
+	return (0);
+}
