1ca155765d6013bdbfdb808c6242abf19c3b211a
jamiu <kabirm474@gmail.com>
2022-07-26 14:55:14 -0700

    frees a 2 dimensional grid previously created

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,10 +8,11 @@
  */
 void free_grid(int **grid, int height)
 {
+
 	if (grid != NULL && height != 0)
-			{
-			for (; height >= 0; height--)
-				free(grid[height]);
-			free(grid);
-			}
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }
