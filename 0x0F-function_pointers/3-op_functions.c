8ba9b7e39eb4710594f0b09f5ac5a6c2aefbb5ce
jamiu <kabirm474@gmail.com>
2022-08-03 08:40:04 -0700

    operation function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: integer with the result of the operation.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract 2 integers
+ * @a: integr 1
+ * @b integr 2
+ * Return: integer with the result of the operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply 2 integers
+ * @a: integr 1
+ * @b: integer 1
+ * Return: integer with the result of the operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: integer with the result of the operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - module of 2 integres
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
