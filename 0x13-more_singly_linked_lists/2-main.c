863d276b10357e193584298a953352eca8f457b6
jamiu <kabirm474@gmail.com>
2022-08-15 13:42:00 -0700

    main

---
+++ 0x13-more_singly_linked_lists/2-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	add_nodeint(&head, 97);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint(head);
+	return (0);
+}
